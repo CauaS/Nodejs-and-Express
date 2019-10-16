ccae0476d7958e5f4218e37cc4e599738ce27c5f
CauaS <37887533+CauaS@users.noreply.github.com>
2019-10-15 23:16:35 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,43 @@ const express = require('express');
 //app recebe o express
 const app = express();
 
+//instancia do mongoose
+const mongoose = require('mongoose');
+
+//body parser para o POST
+const bodyParser = require('body-parser');
+
+//string de conexão
+const url = 'mongodb+srv://caligiuri:cali123@cluster0-vcmdw.mongodb.net/bdTinderDev?retryWrites=true&w=majority';
+
+const options = { 
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500, 
+    poolSize: 5,
+    useNewUrlParser: true,
+}
+
+mongoose.connect(url, options);
+mongoose.set('useCreateIndex', true);
+
+
+mongoose.connection.on('connected', () => {
+    console.log('Conectado!');
+})
+//mosntra erro
+mongoose.connection.on('error', err => {
+    console.log(`Erro na conexão: ${err}`);
+})
+//evento de desconeção
+mongoose.connection.on('disconnected', () => {
+    console.log(`Aplicação desconectada:`);
+})
+
+
+//configuração o body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //instancia os files
 const indexRouter = require ('./Routers/index');
 const usersRouter = require ('./Routers/users');
