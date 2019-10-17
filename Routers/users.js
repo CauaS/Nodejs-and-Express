6a971e6bcd2befd6c3efba98c36d1e570cf23c69
CauaS <37887533+CauaS@users.noreply.github.com>
2019-10-16 23:32:31 -0300

    GET e POST
    Created GET method to retrieve all user from DB.
    
    Created POST method to save a user and verify already existing user at the moment of its creation.

--- Routers/users.js
+++ Routers/users.js
@@ -1,28 +1,53 @@
 const express = require('express');
 const router = express.Router();
 
+const users = require('../model/user');
+
 //endpoints
 
 //http://localhost:3000/?name=Caligiuri&age=23
 
 router.get('/', (req, res) => {
-    const { name, age } = req.query;
+   
+    users.find({}, (err, data)=>{
+        if(err) {
+            return res.send({ error: 'Erro na consulta de usuários'})
+        }
 
-    return res.send({ 
-        message: `Tudo ok com a rota dos usuários`, 
-        name: name, 
-        age: age
-    });
-})
-
-router.post('/', (req, res) => {
-    return res.send({ message: 'Metodo post usuarios'})
-})
+        return res.send(data);
+    })
+});
 
 router.post('/create', (req, res) => {
-    return res.send({
-        message: 'ok create'
-    })
-})
+
+    const { email, password } = req.body;
+
+    if(!email || !password){
+        return res.send({ 
+            message: 'Dados insuficientes!'
+        })
+    }
+
+    users.findOne({ email }, (err, data) => {
+
+        if(err) {
+            return res.send({ erro: 'Erro ao buscar usuário!'})
+        }
+
+        if(data) {
+            return res.send({ erro: 'Usuário ja registrado!'})
+        }
+
+        users.create(req.body, (err, data) => {
+            if(err) {
+                return res.send({ erro: 'Erro ao criar usuário!' })
+            }
+            
+            data.password = undefined; // nao revelar a senha do usuário
+
+            return res.send(data);
+        });
+    });
+});
 
 module.exports = router;
