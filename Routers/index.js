4fc1aba6d316254b5c8e0eeaece061175abda55a
CauaS <37887533+CauaS@users.noreply.github.com>
2019-10-22 22:53:25 -0300

    Add files via upload
    showing hto to call a middleware auth in a require.

--- Routers/index.js
+++ Routers/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middlewares/auth');
+
 //endpoints
 
 //http://localhost:3000/?name=Caligiuri&age=23
 
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
     const { name, age } = req.query;
+    console.log(res.locals.auth_data);
 
     return res.send({ 
         message: `Tudo ok com a rota do index`, 
