4474997f3805c0416444e60327c1c7c288484091
CauaS <37887533+CauaS@users.noreply.github.com>
2019-10-23 22:53:15 -0300

    Add files via upload
    Added status to responses.

--- Routers/users.js
+++ Routers/users.js
@@ -22,35 +22,43 @@ const createUserToken = (userId) => {
 //http://localhost:3000/?name=Caligiuri&age=23
 
 router.get('/', async (req, res) => {
+
+    console.log(req.body);
+    
     try {
         const all_users = await users.find({});
         
         return res.send(all_users);
 
     }catch(e){
-        return res.send({ error: 'Erro na consulta de usuários' })
+        return res.status(500).send({ error: 'Erro na consulta de usuários' })
     }
 });
 
 router.post('/create', async (req, res) => {
     const { email, password } = req.body;
 
+    console.log(email, password);
+
     if(!email || !password){
-        return res.send({ 
-            message: 'Dados insuficientes!'
+        return res.status(400).send({ 
+            message: 'Dados insuficientes!',
+            email : email, 
+            password: password
         });
     }
 
     try {
-        await users.findOne({ email }) ? res.send({ erro: 'Usuário ja registrado!'}) : null;
+        await users.findOne({ email }) ? res.status(400).send({ erro: 'Usuário ja registrado!'}) : null;
 
         const data = await users.create(req.body);
 
         data.password = undefined; // nao revelar a senha do usuário
-        return res.send({ data ,  token: createUserToken(data.id)});
+        
+        return res.status(201).send({ data ,  token: createUserToken(data.id)});
 
     }catch(e){
-        return res.send({ erro: 'Erro ao buscar usuário!'})        
+        return res.status(500).send({ erro: 'Erro ao buscar usuário!'})        
     }
 });
 /*
@@ -58,20 +66,20 @@ router.post('/auth', async (req, res) => {
     const { email, password } = req.body;
 
     if(!email || !password){
-        return res.send({ error: 'Dados Insuficiente!'});
+        return res.status(400).send({ error: 'Dados Insuficiente!'});
     }
     try {
         const data = await users.findOne({ email }).select(+password);
-        !data ?  res.send({ error: 'Usuário não registrado!'}) : null;
+        !data ?  res.status(400).send({ error: 'Usuário não registrado!'}) : null;
 
         const pass_ok = await bcrypt.compare(password, data.password);
-        !pass_ok ?  res.send({ erro: 'Senha incorreta' }) : null;
+        !pass_ok ?  res.status(401).send({ erro: 'Senha incorreta' }) : null;
 
         data.password = undefined;
 
         return res.send({ data ,  token: createUserToken(data.id)});
     }catch(e) {
-        return res.send({ error: 'Erro a buscar usuário!'});
+        return res.status(500).send({ error: 'Erro a buscar usuário!'});
     }
 });
 */
